564ccdc63a389584d043caf2551e7157e1960350
FeliYas <felipe.yaschischin@davinci.edu.ar>
2023-12-05 00:14:54 +0000

    Mensaje de commit

--- main.py
+++ main.py
@@ -5,35 +5,39 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitpal.db'
 db = SQLAlchemy(app)
 
+
 class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  password = db.Column(db.String(80), nullable=False)
+  email = db.Column(db.String(50), unique=True, nullable=False)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+  if request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-        usuario = Usuario.query.filter_by(username=username, password=password).first()
+    usuario = Usuario.query.filter_by(username=username,
+                                      password=password).first()
 
-        if usuario:
-            return redirect(url_for('index'))
-        else:
-            error_message = 'Credenciales incorrectas. Inténtalo de nuevo.'
-            return render_template('login.html', error_message=error_message)
+    if usuario:
+      return redirect(url_for('index'))
+    else:
+      error_message = 'Credenciales incorrectas. Inténtalo de nuevo.'
+      return render_template('login.html', error_message=error_message)
 
-    return render_template('login.html')
+  return render_template('login.html')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
+
 
 with app.app_context():
-    db.create_all()
+  db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+  app.run(debug=True, host='0.0.0.0')
